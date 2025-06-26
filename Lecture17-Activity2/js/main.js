b1c8546cd483b765c50dbb792a13a58fe9ff5c00
Hansenyao <yyfsingler@163.com>
2025-06-25 22:25:11 -0600

    add Lecture7 activities

---
+++ Lecture17-Activity2/js/main.js
@@ -0,0 +1,7 @@
+
+/* We add an event listener to the element with id "display-mssage" */
+document.querySelector("#display-message").addEventListener("click", () =>{
+
+    console.log("Testing Javascript");
+
+})
